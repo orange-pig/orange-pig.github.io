4b6e80d60b1b8cba338e927c4132b4b128f94d7c
orange-pig <sitandevil@gmail.com>
2015-03-26 17:35:15 +0800

    Add a new page with camare test
    i find that, this is support jacascript & bootstrap

---
+++ _site/Scripts/camare.js
@@ -0,0 +1,27 @@
+﻿function camera(element) {
+    var videoObj = { video: true };
+
+    if (!window.URL) {
+        window.URL = window.URL || window.webkitURL || window.msURL|| window.mozURL || window.oURL;
+    }
+    if (!navigator.getUserMedia) {
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    }
+
+    window.navigator.getUserMedia(
+        videoObj,
+        function(stream){
+            try{
+                element.src = window.URL.createObjectURL(stream);
+                element.play();
+            } catch (err) {
+                element.src = stream;
+                element.play();
+            }
+        },
+        function(){
+            throw Error('未发现摄像头');
+        }
+    );
+}
